42a46cd2a0b445acca45017f7bfebc67c2ab5dc8
Wen Li <wenlilearn@gmail.com>
2018-03-17 02:07:54 -0400

    datatypes section

---
+++ datatypes/app.js
@@ -0,0 +1,39 @@
+//string
+const name = 'john doe';
+console.log(typeof name);
+
+//number
+const age = 45;
+console.log(typeof age);
+
+//boolean
+const has_kids = true;
+console.log(typeof has_kids);
+
+//null
+const car = null;
+console.log(typeof car);
+
+// undefined
+let test;
+console.log(typeof test);
+
+//symbol
+const sym = Symbol();
+console.log(typeof sym);
+
+//Ref types
+//array
+const hobbies = ['movies', 'music'];
+console.log(typeof hobbies);
+
+//object literal
+const address = {
+  city: 'boston',
+  state: 'MA'
+}
+const today = new Date();
+
+console.log(typeof address);
+console.log(today);
+console.log(typeof today);
---
+++ datatypes/readme.md
@@ -0,0 +1,7 @@
+### Primitive data types:
+- String, number, boolean, null, undefined, symbols(ES 6)
+
+### Reference data types:
+- Arrays, Object Literals, Functions, Dates, Anything else
+
+### Dynamic Typed language
